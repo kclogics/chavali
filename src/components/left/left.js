86c44cdcfa1df322f98180b2ebc11cc388b4b5dd
kclogics <kclogics@gmail.com>
2019-04-29 02:08:46 +0530

    low fidelity wireframing of the whole container

--- src/components/left/left.js
+++ src/components/left/left.js
@@ -5,9 +5,9 @@ import "../../App.css";
 function Left() {
   return (
     <div className="left">
-      {/* <div className="LeftElementTags"> Tags</div> */}
       <div className="LeftElementHeader"> Header</div>
       <div className="LeftElementBody">
+        <div className="LeftElementContent">LContent</div>
         <div className="LeftElementTags">Tags</div>
       </div>
       <div className="LeftElementFooter"> footer</div>
--- src/components/right/right.js
+++ src/components/right/right.js
@@ -3,7 +3,16 @@ import React from "react";
 import "../../App.css";
 
 function Right() {
-  return <div className="right">hello </div>;
+  return (
+    <div className="right">
+      <div className="RightElementHeader"> Header</div>
+      <div className="RightElementBody">
+        <div className="RightElementContent">RContent</div>
+        <div className="RightElementTags">Tags</div>
+      </div>
+      <div className="RightElementFooter"> footer</div>
+    </div>
+  );
 }
 
 export default Right;
